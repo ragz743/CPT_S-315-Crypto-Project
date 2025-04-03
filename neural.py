22a4cd4edc9191a18704b67d53ce49cf75bbda6d
darronese <darron198@hotmail.com>
2025-04-03 15:26:26 -0700

    removed headers

--- neural.py
+++ neural.py
@@ -1,8 +1,13 @@
-import numpy as np
-import pandas as pd
 import torch
 
 
 def tensor(processed_data, sequence_data_x, sequence_data_y):
+
+    # set our input data as tensor
     x_np = torch.from_numpy(sequence_data_x)
+    print("X TENSOR: ", x_np)
     y_np = torch.from_numpy(sequence_data_y)
+    print("Y TENSOR: ", y_np)
+
+    # build our initial model
+
--- sequence.py
+++ sequence.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-from scipy.stats import zscore
 
 
 # process bitcoin data into sorted dates
